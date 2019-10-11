f2a547bfc6d78fbd7a27c2c5b374a53e5e51d630
Admaassu <admassu2005@gmail.com>
2019-10-11 12:17:51 -0400

    last commit

---
+++ src/main/java/com/example/demo/Message.java
@@ -0,0 +1,59 @@
+package com.example.demo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
+import javax.validation.constraints.Size;
+
+public class Message {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @NotNull
+    @Size(min = 10)
+    private String content;
+
+    @NotNull
+    @Size(min = 6)
+    private String posted_date;
+
+    @Null
+    @Size(min = 8)
+    private String posted_by;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getPosted_date() {
+        return posted_date;
+    }
+
+    public void setPosted_date(String posted_date) {
+        this.posted_date = posted_date;
+    }
+
+    public String getPosted_by() {
+        return posted_by;
+    }
+
+    public void setPosted_by(String posted_by) {
+        this.posted_by = posted_by;
+    }
+}
